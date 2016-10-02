91c7198623e0f6d45faa8e1e92842d92ac5ef8ce
nive18 <niveditha.itengineer@gmail.com>
2016-10-03 02:37:42 +0530

    Create Takeabreak.py

---
+++ Takeabreak.py
@@ -0,0 +1,22 @@
+import webbrowser
+import time
+
+
+total_breaks=4
+break_count=0
+while(break_count<total_breaks):
+    if(break_count==0):
+        time.sleep(1500)
+        webbrowser.open("https://www.youtube.com/watch?v=zOYxXWsogVA")
+    if(break_count==1):
+        time.sleep(1800)
+        webbrowser.open("https://www.youtube.com/watch?v=vYdy_bsva78")
+    if(break_count==2):
+        time.sleep(1500)
+        webbrowser.open("https://www.youtube.com/watch?v=0QfOmMHj6EM")
+    if(break_count==3):
+        timr.sleep(1800)
+        webbrowser.open("https://www.youtube.com/watch?v=wLDt22Xw6lI")
+
+    
+    break_count=break_count+1
