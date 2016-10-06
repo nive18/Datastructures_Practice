2061f6c606b18f5fe1c05bb0f2b047a06ffc9729
nive18 <niveditha.itengineer@gmail.com>
2016-10-06 20:23:36 +0530

    Update Multiplicationtable.java

--- Multiplicationtable.java
+++ Multiplicationtable.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 	Scanner sc=new Scanner(System.in);
 	int n,i,k;
 	n=sc.nextInt();
-	for(i=1;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
 		k=n*i;
 		System.out.println(k);
