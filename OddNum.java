d1c91ce11960c831947a8ea2524553e8187c684d
nive18 <niveditha.itengineer@gmail.com>
2016-10-06 19:20:25 +0530

    Add files via upload

---
+++ OddNum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class OddNum {
+public static void main(String args[])
+{
+	int i,k,l,n;
+	boolean flag=false;
+    Scanner sc=new Scanner(System.in);
+    
+    k=sc.nextInt();
+    l=sc.nextInt();
+    for(i=k;i<l;i++)
+    {
+    	if(i%2!=0)
+    	{
+        	System.out.println(i);
+    	}
+    	
+    }
+    
+}
+}
