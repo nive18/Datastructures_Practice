08acfa60adbee036f79337e17c52db79cf396d06
nive18 <niveditha.itengineer@gmail.com>
2016-10-06 20:32:22 +0530

    Add files via upload

---
+++ Factorial1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Factorial1 {
+public static void main(String args[])
+{
+	int n,i;
+	Scanner sc=new Scanner(System.in);
+	int fact=1;
+	n=sc.nextInt();
+	for(i=1;i<=n;i++)
+	{
+		fact=fact*i;
+	}
+	if(fact==0||fact==1)
+	{
+		System.out.println(fact);
+	}
+	else
+	{
+		System.out.println(fact);
+	}
+}
+}
