8728ed5a33308b034e3a1a7cd40f13fa98bcb229
nive18 <niveditha.itengineer@gmail.com>
2016-10-07 19:18:33 +0530

    Add files via upload

---
+++ Fibanocci.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Fibanocci {
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	int n,n1=0,n2=1,n3,i;
+	n=sc.nextInt();
+	System.out.print(n1+" "+n2);
+	for(i=2;i<n;i++)
+	{
+		n3=n1+n2;
+		System.out.print(" "+n3);
+		n1=n2;
+		n2=n3;
+	}
+	
+}
+}
